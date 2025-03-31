cf85b1af3d546f54b06f101cab02dc141ddee26c
Nishant-jswl <152166858+Uknowme-h@users.noreply.github.com>
2025-03-31 10:11:59 +0545

    initialized backend with hosted auth api

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://20.55.20.86:5000/api/auth" : "/api/auth";
 const CLOUD_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/upload" : "/api/upload";
 
 
@@ -17,10 +17,10 @@ export const useAuthStore = create((set) => ({
     message: null,
     audioSrc: null,
 
-    signup: async (email, password, name) => {
+    signup: async (email, password, name, userType) => {
         set({ isLoading: true, error: null });
         try {
-            const response = await axios.post(`${API_URL}/signup`, { email, password, name });
+            const response = await axios.post(`${API_URL}/signup`, { email, password, name, userType });
             set({ user: response.data.user, isAuthenticated: true, isLoading: false });
         } catch (error) {
             set({ error: error.response.data.msg || "Error signing up", isLoading: false });
