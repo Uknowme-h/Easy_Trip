d09944a5aa814a3715e96eb30b4f59563503e369
Nishant-jswl <152166858+Uknowme-h@users.noreply.github.com>
2025-03-31 10:14:59 +0545

    initialized backend with hosted auth api

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 
 
-const API_URL = import.meta.env.MODE === "development" ? "http://20.55.20.86:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://20.55.20.86:5000/api/auth" : "http://20.55.20.86:5000/api/auth/api/auth";
 const CLOUD_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/upload" : "/api/upload";
 
 
