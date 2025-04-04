88c397356803beb5b17a2985f8331b6361ceb1c5
Nishant-jswl <152166858+Uknowme-h@users.noreply.github.com>
2025-04-04 20:09:23 +0545

    fixed cookie setting problem

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,21 +1,15 @@
-import jwt from 'jsonwebtoken';
-
-
 export const generateTokenAndSetCookie = (res, id) => {
     const token = jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
 
     const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-
-    }
-
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+        sameSite: 'none',
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    };
 
     res.cookie('token', token, cookieOptions);
     return token;
-}
+};
