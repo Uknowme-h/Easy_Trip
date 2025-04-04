33f1bd9479d9b8fac5b6284b3a5c2159e5609379
Ubuntu <apple@myVm.0uaibx3x4ujuboz1ex0lbqbtch.bx.internal.cloudapp.net>
2025-04-04 18:26:26 +0000

    fixed cookies

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -9,12 +9,13 @@ export const generateTokenAndSetCookie = (res, id) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        secure: true
 
     }
 
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
 
     res.cookie('token', token, cookieOptions);
     return token;
